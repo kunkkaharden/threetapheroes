c35c01f6d292bbd0a3364ed8c0a6d8602c14ad50
Adrian Gómez Reyes <adrigoreyes@gmail.com>
2020-11-27 21:52:39 +0100

    mis ca

--- _token.js
+++ _token.js
@@ -1,10 +1,10 @@
 module.exports = {
     //Put your bot related informations here
-    APP_NAME : "MyPrettyBot",
-    BOT_TOKEN : "MyAwesomeToken",
-    WEBHOOK : 'https://myawesomeurl.whatever', //https://telegram-bot-sdk.readme.io/docs/setwebhook
-    GAME_URL : 'https://mycoolgame.gg',
-    GAME_NAME : 'mycoolgame',
+    APP_NAME : "buscaminas",
+    BOT_TOKEN : "1378078685:AAHncHBpFedJh68SxJURVOc7ZnQVBGOPgkk",
+    WEBHOOK : 'https://app-boteandogames.herokuapp.com/',
+    GAME_URL : 'https://app-boteandogames.herokuapp.com/index.html',
+    GAME_NAME : 'buscaminas',
     PORT : 3000,    // Put your specific port here
     DEFAULT_PORT : 3000
 }
